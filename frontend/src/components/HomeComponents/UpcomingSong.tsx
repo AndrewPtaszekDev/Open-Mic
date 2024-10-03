5c0761b448e321ef2b17ad7a7f975b755b5e1d15
Epicskulcrusher <soprano19522@gmail.com>
2024-10-03 11:35:22 -0400

    New look to the upcoming songs and song request

--- frontend/src/components/HomeComponents/UpcomingSong.tsx
+++ frontend/src/components/HomeComponents/UpcomingSong.tsx
@@ -1,17 +1,18 @@
-import './UpcomingSong.css';
+import './UpcomingSong.css';  // Make sure this path is correct
 import React from 'react';
 
 interface UpcomingSongInterface {
     fullName: string,
     songName: string
 }
-const UpcomingSong: React.FC<UpcomingSongInterface> = ({fullName, songName}) => {
-   return (
-       <div className="upcoming-song">
-           <div className='upcoming-fullname'>{fullName}</div>
-           <div className='upcoming-songname'>{songName}</div>
-       </div>
-   );
+
+const UpcomingSong: React.FC<UpcomingSongInterface> = ({ fullName, songName }) => {
+    return (
+        <div className="upcoming-song">  {/* Make sure this class matches your CSS */}
+            <div className="upcoming-fullname">{fullName}</div>  {/* Same here */}
+            <div className="upcoming-songname">{songName}</div>  {/* And here */}
+        </div>
+    );
 };
 
 export default UpcomingSong;
--- frontend/src/components/NavBarComponents/SiteTitle.tsx
+++ frontend/src/components/NavBarComponents/SiteTitle.tsx
@@ -5,7 +5,7 @@ const SiteTitle: React.FC = () => {
   return (
     <div className="site-title">
         <div className="site-title-text">
-            The Open Mic Site
+            Welcome to Open Mic Night
         </div>
     </div>
   );
