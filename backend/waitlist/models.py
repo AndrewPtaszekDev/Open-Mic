9d682ee5f1e062f1cfdba14c3a08b3bc5db9f110
VenaFL <andrewptaszek@gmail.com>
2024-10-02 21:46:48 -0400

    added create_entry_admin endpoint

--- backend/waitlist/models.py
+++ backend/waitlist/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, transaction
 
 
 # Create your models here
@@ -10,9 +10,13 @@ class WaitlistModel(models.Model):
     def __str__(self):
         return self.name
     
-    def save(self, *args, **kwargs):
-        # If it's a new instance (doesn't have an id yet)
-        if self._state.adding:
+    def save(self, *args, custom_order=None, **kwargs):
+
+        if custom_order is not None:
+            self.order = custom_order
+
+        # If it's a new instance 
+        elif self._state.adding:
             # Get the current maximum order value, increment by 1
             last_order = WaitlistModel.objects.all().aggregate(models.Max('order'))['order__max']
             if last_order is None:
@@ -20,3 +24,8 @@ class WaitlistModel(models.Model):
             self.order = last_order + 1
         super().save(*args, **kwargs)
     
+    @classmethod
+    def increment_orders_from(cls, threshold):
+        # Increment the order of all entries from the given threshold to highest order value
+        with transaction.atomic(): # If one fails, it all fails
+            cls.objects.filter(order__gte=threshold).update(order=models.F('order') + 1)
