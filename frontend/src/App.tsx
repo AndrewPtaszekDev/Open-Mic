f8d37e5aad90d4e353ba5034ce51fc1ef5890400
Zane Wolfe <85024901+Zane-Wolfe@users.noreply.github.com>
2024-10-05 02:03:00 -0400

    Adding a song now instantly updates UI, and added alert for successful POST request

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-//import NavBar from "./components/NavBarComponents/NavBar";
 import './App.css';
 import SiteTitle from "./components/HomeComponents/SiteTitle";
 import Home from "./components/HomeComponents/Home";
@@ -9,18 +8,8 @@ import ScrollingBanner from './components/ScrollingBanner/ScrollingBanner';
 import Admin from "./components/AdminComponents/Admin";
 import NavMenu from './components/NavBarComponents/NavMenu';
 import Admin_Interface from "./components/AdminComponents/Admin_Interface";
-
-const About: React.FC = () => (
-	<div>
-		<h1>About Page</h1>
-	</div>
-);
-
-const Events: React.FC = () => (
-  <div>
-    <h1>Events Page</h1>
-  </div>
-);
+import Club from "./components/ClubComponents/Club";
+import About from "./components/AboutComponents/About";
 
 const App: React.FC = () => {
   const names = [ 'Our Dev Team', 'Andrew Ptaszek', 'Zane Wolfe', 'Colin Sadowitz', 'Michael Carlson',
@@ -28,27 +17,24 @@ const App: React.FC = () => {
   'Our Dev Team', 'Andrew Ptaszek', 'Zane Wolfe', 'Colin Sadowitz', 'Michael Carlson'];
   // Names for banner stored here
 
-
   return (
     <Router>
         <div/>
         {/* Scrolling Banner */}
        <ScrollingBanner names={names} />
-
         {/* Main Content */}
         <div>
-          {/* Off-Canvas NavMenu */}
-          <NavMenu />
-          
-          {/* Routes for different pages */}
-          <Routes>
+        {/* Off-Canvas NavMenu */}
+        <NavMenu />
+        {/* Routes for different pages */}
+        <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/club" element={<Club />} />
           <Route path="/about" element={<About />} />
-          <Route path="/events" element={<Events />} />
-	  <Route path="/admin-page" element={<Admin />} />
-	  <Route path="/admin-interface" element={<Admin_Interface/>} />
-          </Routes>
-      </div>
+    	  <Route path="/admin-page" element={<Admin />} />
+          <Route path="/admin-interface" element={<Admin_Interface/>} />
+        </Routes>
+        </div>
     </Router>
   );
 };
--- frontend/src/components/HomeComponents/Home.tsx
+++ frontend/src/components/HomeComponents/Home.tsx
@@ -1,19 +1,41 @@
 import './Home.css';
-import React from 'react';
+import React, {useState} from 'react';
 import SongRequests from "./SongRequests";
 import CurrentWaitlist from "./CurrentWaitlist";
 import SiteTitle from './SiteTitle';
+import axios from "axios";
+
+
+// Define the expected shape of data
+export interface WaitlistItem {
+  name: string;
+  song: string;
+}
+
 
 const Home: React.FC = () => {
+
+  const [data, setData] = useState<WaitlistItem[]>([]); // Use an array of objects for the waitlist data
+    // Function to fetch data from the backend
+    const fetchData = () => {
+        axios.get('http://127.0.0.1:8000/api/get_all_waitlists/')
+            .then((response) => {
+                setData(response.data); // Assuming response.data is an array of {name, song} objects
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    };
+
   return (
       <div className="home">
           <div className="border">
              <SiteTitle />
-             <SongRequests/>
+             <SongRequests fetchData={fetchData}/>
           </div>
           <div className='border'>
               <div>Songs Up Next:</div>
-              <CurrentWaitlist/>
+              <CurrentWaitlist fetchData={fetchData} data={data}/>
           </div>
       </div>
   );
