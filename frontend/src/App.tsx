0ee81987d1c1ac723a3c1600c4e48c113ed5a9c4
Epicskulcrusher <soprano19522@gmail.com>
2024-10-17 11:03:38 -0400

    New Heading Component
    I added a heading component to fix the issues with its formatting. It contains both the NavMenu and the ScrollingBanner components. Also played around with a couple of other formating compoents on the home page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,44 +2,36 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
-import SiteTitle from "./components/HomeComponents/SiteTitle";
 import Home from "./components/HomeComponents/Home";
-import ScrollingBanner from './components/ScrollingBanner/ScrollingBanner';
+import Heading from "./components/HeadingComponents/Heading"; // Import the new Heading component
 import Admin from "./components/AdminComponents/Admin";
-import NavMenu from './components/NavBarComponents/NavMenu';
 import Admin_Interface from "./components/AdminComponents/Admin_Interface";
 import Club from "./components/ClubComponents/Club";
 import About from "./components/AboutComponents/About";
-import { PasswordContext } from './components/AdminComponents/Password_Context' // access password site wide
+import { PasswordContext } from './components/AdminComponents/Password_Context'; // Access password site wide
 
 const App: React.FC = () => {
-  const names = [ 'Our Dev Team', 'Andrew Ptaszek', 'Zane Wolfe', 'Colin Sadowitz', 'Michael Carlson',
-  'Our Dev Team', 'Andrew Ptaszek', 'Zane Wolfe', 'Colin Sadowitz', 'Michael Carlson',
-  'Our Dev Team', 'Andrew Ptaszek', 'Zane Wolfe', 'Colin Sadowitz', 'Michael Carlson'];
-  // Names for banner stored here
+  const names = [
+    'Our Dev Team', 'Andrew Ptaszek', 'Zane Wolfe', 'Colin Sadowitz', 'Michael Carlson','Our Dev Team', 'Andrew Ptaszek', 'Zane Wolfe', 'Colin Sadowitz', 'Michael Carlson','Our Dev Team', 'Andrew Ptaszek', 'Zane Wolfe', 'Colin Sadowitz', 'Michael Carlson'
+  ]; // Names for the banne
 
   const [password, setPassword] = useState('');
 
   return (
     <PasswordContext.Provider value={{ password, setPassword }}>
-    <Router>
-        <div/>
-        {/* Scrolling Banner */}
-       <ScrollingBanner names={names} />
-        {/* Main Content */}
+      <Router>
+        <Heading names={names} />
+
         <div>
-        {/* Off-Canvas NavMenu */}
-        <NavMenu />
-        {/* Routes for different pages */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/club" element={<Club />} />
-          <Route path="/about" element={<About />} />
-    	  <Route path="/admin-page" element={<Admin />} />
-          <Route path="/admin-interface" element={<Admin_Interface/>} />
-        </Routes>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/club" element={<Club />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/admin-page" element={<Admin />} />
+            <Route path="/admin-interface" element={<Admin_Interface />} />
+          </Routes>
         </div>
-    </Router>
+      </Router>
     </PasswordContext.Provider>
   );
 };
