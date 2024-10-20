25311144c418b776a8376b4dc5f916c25c5c091a
Epicskulcrusher <soprano19522@gmail.com>
2024-10-20 15:54:11 -0400

    Finished Club and About page.

--- frontend/src/components/AboutComponents/About.tsx
+++ frontend/src/components/AboutComponents/About.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 const About: React.FC = () => {
   return (
     <div>
-    <div className="clubtitle">
+    <div className="aboutclubtitle">
       The Production Team
     </div>
-    <div className="text">
-      This is some other example text.
+    <div className="aboutcontainer">
+      <div className="abouttext">
+      Our team created a website as part of our learning journey into React, Django, and backend server development. The main purpose of this website is to replace the manual list we currently use for our events, which keeps track of the order in which people go up to perform. By moving this process online, we’ve aimed to make it more efficient and organized, allowing users to sign up digitally and view their position in the performance lineup in real-time. This project has been a great way to build our skills in both front-end and back-end development, while creating a useful tool for event management.
+     </div>
     </div>
   </div>
   );
--- frontend/src/components/ClubComponents/Club.tsx
+++ frontend/src/components/ClubComponents/Club.tsx
@@ -5,14 +5,18 @@ const Club: React.FC = () => {
   return (
     <div>
       <div className="clubtitle">
-      The purpose of the Music Association is to connect poly students interested in music. We exist to encourage the expression of musical talents, foster collaboration between musicians and music lovers, and assist in developing musical abilities for all students here at Florida Poly. Along with jam sessions, the club hosts workshops to teach and explore music-related topics such as music theory, production, and performance techniques.
+        Music Association
       </div>
-      <div className="text">
-        Join our discord server: https://discord.gg/QV9txczfVd
-      </div>
-        <div>
-          
+      <div className="clubcontainer">
+        <div className="clubtext">
+          The purpose of the Music Association is to connect poly students interested in music. We exist to encourage the expression of musical talents, foster collaboration between musicians and music lovers, and assist in developing musical abilities for all students here at Florida Poly. Along with jam sessions, the club hosts workshops to teach and explore music-related topics such as music theory, production, and performance techniques.
         </div>
+      </div>
+      <div className="clubcontainer">
+        <div className="clubtext">
+          Join our discord server: <a href='https://discord.gg/QV9txczfVd'>here</a>.
+        </div>
+      </div>
     </div>
   );
 };
