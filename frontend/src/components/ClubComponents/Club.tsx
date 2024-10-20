2271beb6a93337e7257f7b984a27bd58b8ca4bb9
Andrew Ptaszek <andrewptaszek@gmail.com>
2024-10-20 15:28:38 -0400

    updates to club info

--- frontend/src/components/ClubComponents/Club.tsx
+++ frontend/src/components/ClubComponents/Club.tsx
@@ -5,11 +5,14 @@ const Club: React.FC = () => {
   return (
     <div>
       <div className="clubtitle">
-        The Music Association
+      The purpose of the Music Association is to connect poly students interested in music. We exist to encourage the expression of musical talents, foster collaboration between musicians and music lovers, and assist in developing musical abilities for all students here at Florida Poly. Along with jam sessions, the club hosts workshops to teach and explore music-related topics such as music theory, production, and performance techniques.
       </div>
       <div className="text">
-        This is some example text.
+        Join our discord server: https://discord.gg/QV9txczfVd
       </div>
+        <div>
+          
+        </div>
     </div>
   );
 };
