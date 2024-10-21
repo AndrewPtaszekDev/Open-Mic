2eea0a1326949bfe9b6159083ebac1923c611d8c
Andrew Ptaszek <andrewptaszek@gmail.com>
2024-10-21 09:44:23 -0400

    updated club and about

--- frontend/src/components/AboutComponents/About.tsx
+++ frontend/src/components/AboutComponents/About.tsx
@@ -7,9 +7,17 @@ const About: React.FC = () => {
     <div className="aboutclubtitle">
       The Production Team
     </div>
+    <div className='aboutcontainer'>
+      <div className='abouttext' >
+        Andrew Ptaszek - Project Manager & Backend Developer <br></br>
+        Zane Wolfe - Fullstack Developer <br></br>
+        Colin Sadowitz - Frontend Engineer <br></br>
+        Michael Carlson - UI/UX Designer <br></br>
+      </div>
+    </div>
     <div className="aboutcontainer">
       <div className="abouttext">
-      Our team created a website as part of our learning journey into React, Django, and backend server development. The main purpose of this website is to replace the manual list we currently use for our events, which keeps track of the order in which people go up to perform. By moving this process online, we’ve aimed to make it more efficient and organized, allowing users to sign up digitally and view their position in the performance lineup in real-time. This project has been a great way to build our skills in both front-end and back-end development, while creating a useful tool for event management.
+      Our team created a website as part of our learning journey into web development using React and Django. The main purpose of this website is to replace the manual list we currently use for our events, which keeps track of the performance order. By moving this process online, we’ve aimed to make it more efficient and organized, allowing users to sign up digitally and view their position in the performance lineup in real-time. This project has been a great way to build our skills in both front-end and back-end development, while creating a useful tool for event management.
      </div>
     </div>
   </div>
--- frontend/src/components/ClubComponents/Club.tsx
+++ frontend/src/components/ClubComponents/Club.tsx
@@ -16,6 +16,25 @@ const Club: React.FC = () => {
         <div className="clubtext">
           Join our discord server: <a href='https://discord.gg/QV9txczfVd'>here</a>.
         </div>
+        
+        <div className="clubtext">
+          Join the club: <a href='https://floridapoly.presence.io/organization/music-association'>here</a>.
+        </div>
+      </div>
+      <div className="clubcontainer">
+        <div className="clubtext">
+          President: Lukas Kelk <br></br>
+          Vice President: Andrew Ptaszek <br></br>
+          Secretary: Carlos Murillo <br></br>
+          Treasurer: Jake Diaz-Iglesias <br></br>
+        </div>
+      </div>
+      <div className="clubcontainer">
+        <div className="clubtext">
+          Club Meetings (SIM Lab, IST-1022):<br></br>
+          Jam Session 4:00 - 6:30pm, Thursdays <br></br>
+          Music Theory Workshop 6:30 - 7:30pm, Thursdays <br></br>
+        </div>
       </div>
     </div>
   );
