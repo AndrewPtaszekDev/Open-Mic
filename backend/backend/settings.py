e74dee1923c0df3de998580cd83509e7d3edc7c9
VenaFL <andrewptaszek@gmail.com>
2024-11-09 19:52:13 -0500

    debug to false

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 ***REMOVED***
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['34.74.108.77', '34.74.108.77:8000', '34.74.108.77:3000']
 
